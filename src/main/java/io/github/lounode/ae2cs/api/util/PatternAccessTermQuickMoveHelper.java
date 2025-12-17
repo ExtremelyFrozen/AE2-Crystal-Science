00370f2472ccd919f29551b47bab002e7078c8b9
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-12-17 19:42:54 +0800

    修复Item对不上的小错误

--- src/main/java/io/github/lounode/ae2cs/api/util/PatternAccessTermQuickMoveHelper.java
+++ src/main/java/io/github/lounode/ae2cs/api/util/PatternAccessTermQuickMoveHelper.java
@@ -12,7 +12,7 @@ public class PatternAccessTermQuickMoveHelper
 
     public static void init()
     {
-        add(AECSBlocks.METEORITE_CRAFTER_BLOCK);
+        add(AECSBlocks.METEORITE_CRAFTER_BLOCK.asItem());
     }
 
     public static void add(ItemLike item)
