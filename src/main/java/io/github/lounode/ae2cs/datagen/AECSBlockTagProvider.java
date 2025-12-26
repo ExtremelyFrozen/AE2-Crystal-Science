79bd786ae145c917481c40abbcea0f71ac4bcdae
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-12-26 19:14:10 +0800

    允许广播装置成为信标基座

--- src/main/java/io/github/lounode/ae2cs/datagen/AECSBlockTagProvider.java
+++ src/main/java/io/github/lounode/ae2cs/datagen/AECSBlockTagProvider.java
@@ -1,8 +1,10 @@
 package io.github.lounode.ae2cs.datagen;
 
 import io.github.lounode.ae2cs.api.ids.AECSConstants;
+import io.github.lounode.ae2cs.common.init.AECSBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -21,5 +23,7 @@ public class AECSBlockTagProvider extends BlockTagsProvider
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider)
     {
+        tag(BlockTags.BEACON_BASE_BLOCKS)
+                .add(AECSBlocks.ENDER_BROADCASTER_BLOCK.get());
     }
 }
