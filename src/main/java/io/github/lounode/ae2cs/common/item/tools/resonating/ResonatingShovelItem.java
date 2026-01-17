811c2334d2b691cf3c86c531dd610d100909b9c8
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-01-17 16:32:29 +0800

    修复谐振工具的精准采集问题

--- src/main/java/io/github/lounode/ae2cs/common/item/tools/resonating/ResonatingShovelItem.java
+++ src/main/java/io/github/lounode/ae2cs/common/item/tools/resonating/ResonatingShovelItem.java
@@ -7,12 +7,14 @@ import io.github.lounode.ae2cs.common.item.tools.AECSToolType;
 import io.github.lounode.ae2cs.common.item.tools.IntrinsicEnchantment;
 import io.github.lounode.ae2cs.common.item.tools.LinkableTool;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
@@ -51,6 +53,23 @@ public class ResonatingShovelItem extends ShovelItem implements LinkableTool, In
     public int getIntrinsicEnchantLevel(ItemStack stack, Holder<Enchantment> enchantment)
     {
         return intrinsicEnchantment.getLevel(enchantment);
+    }
+
+    @Override
+    public @NotNull ItemEnchantments getAllEnchantments(@NotNull ItemStack stack, HolderLookup.@NotNull RegistryLookup<Enchantment> lookup)
+    {
+        // 精准采集只认这一条路径
+        // 也许后续可以考虑把精准采集的附魔原生写入，不过这样会影响到tooltip和其他东西
+        ItemEnchantments base = super.getAllEnchantments(stack, lookup);
+        var opt = lookup.get(Enchantments.SILK_TOUCH);
+        if (opt.isEmpty()) return base;
+
+        var silk = opt.get();
+        if (base.getLevel(silk) >= 1) return base;
+
+        ItemEnchantments.Mutable mutable = new ItemEnchantments.Mutable(base);
+        mutable.set(silk, 1);
+        return mutable.toImmutable();
     }
 
     @Override
