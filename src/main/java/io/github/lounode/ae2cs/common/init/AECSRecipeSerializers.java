bf22ba2e1f7a04f7766f543a74577f99e1cd12fa
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-02-23 01:49:30 +0800

    移植io/github/lounode/ae2cs/common/init/AECSRecipeSerializers.java

--- src/main/java/io/github/lounode/ae2cs/common/init/AECSRecipeSerializers.java
+++ src/main/java/io/github/lounode/ae2cs/common/init/AECSRecipeSerializers.java
@@ -8,18 +8,18 @@ import io.github.lounode.ae2cs.common.recipe.crystal_aggregator.CrystalAggregato
 import io.github.lounode.ae2cs.common.recipe.crystal_aggregator.CrystalAggregatorRecipeSerializer;
 import io.github.lounode.ae2cs.common.recipe.crystal_pulverizer.CrystalPulverizerRecipe;
 import io.github.lounode.ae2cs.common.recipe.crystal_pulverizer.CrystalPulverizerRecipeSerializer;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Supplier;
 
 public class AECSRecipeSerializers
 {
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS =
-            DeferredRegister.create(Registries.RECIPE_SERIALIZER, AECSConstants.MODID);
+            DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, AECSConstants.MODID);
 
     public static final Supplier<RecipeSerializer<CircuitEtcherRecipe>> CIRCUIT_ETCHER =
             RECIPE_SERIALIZERS.register("circuit_etcher_recipe_serializer", CircuitEtcherRecipeSerializer::new);
