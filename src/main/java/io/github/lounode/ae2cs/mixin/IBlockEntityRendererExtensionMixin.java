397434f4b72ba49501a1748f777481d3c000f2e2
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-02-24 02:00:40 +0800

    添加mixinextras，移植以及移除一些Mixin类

--- src/main/java/io/github/lounode/ae2cs/mixin/IBlockEntityRendererExtensionMixin.java
+++ src/main/java/io/github/lounode/ae2cs/mixin/IBlockEntityRendererExtensionMixin.java
@@ -2,31 +2,25 @@ package io.github.lounode.ae2cs.mixin;
 
 import appeng.api.parts.IPart;
 import appeng.blockentity.networking.CableBusBlockEntity;
-import appeng.blockentity.networking.CableBusTESR;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import io.github.lounode.ae2cs.api.render.ICustomRenderBounding;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.AABB;
-import net.neoforged.neoforge.client.extensions.IBlockEntityRendererExtension;
+import net.minecraftforge.common.extensions.IForgeBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = IBlockEntityRendererExtension.class, remap = false)
+@Mixin(value = IForgeBlockEntity.class, remap = false)
 public interface IBlockEntityRendererExtensionMixin
 {
     @ModifyReturnValue(
-            method = "getRenderBoundingBox(Lnet/minecraft/world/level/block/entity/BlockEntity;)Lnet/minecraft/world/phys/AABB;",
+            method = "getRenderBoundingBox()Lnet/minecraft/world/phys/AABB;",
             at = @At("RETURN")
     )
-    private AABB aecs$getRenderBoundingBox(AABB original, BlockEntity be)
+    private AABB aecs$getRenderBoundingBox(AABB original)
     {
-        if (!(this instanceof CableBusTESR))
-        {
-            return original;
-        }
-        if (!(be instanceof CableBusBlockEntity te))
+        if (!(this instanceof CableBusBlockEntity te))
         {
             return original;
         }
