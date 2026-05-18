4d14d5a98e3cc181fee6267b3151d5744ba1319e
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-05-18 22:51:09 +0800

    修复model datagen

--- src/main/java/io/github/lounode/ae2cs/datagen/AECSModelProvider.java
+++ src/main/java/io/github/lounode/ae2cs/datagen/AECSModelProvider.java
@@ -17,11 +17,9 @@ import io.github.lounode.ae2cs.datagen.properties.GrowProcess;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
-import net.minecraft.client.data.models.MultiVariant;
 import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
 import net.minecraft.client.data.models.blockstates.PropertyDispatch;
 import net.minecraft.client.data.models.model.*;
-import net.minecraft.client.renderer.block.dispatch.Variant;
 import net.minecraft.client.renderer.block.dispatch.VariantMutator;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.Direction;
@@ -55,12 +53,12 @@ public class AECSModelProvider extends ModelProvider {
         blockWithItem(blockModels, AECSBlocks.DEEPSLATE_CERTUS_QUARTZ_ORE);
         blockWithItem(blockModels, AECSBlocks.CHARGED_CERTUS_QUARTZ_ORE);
         blockWithItem(blockModels, AECSBlocks.DEEPSLATE_CHARGED_CERTUS_QUARTZ_ORE);
-        genPatternProviderLike(blockModels, AECSBlocks.INTEGRATED_INTERFACE_BLOCK);
-        genPatternProviderLike(blockModels, AECSBlocks.EX_INTEGRATED_INTERFACE_BLOCK);
-        genPatternProviderLike(blockModels, AECSBlocks.METEORITE_PATTERN_PROVIDER_BLOCK);
-        genPatternProviderLike(blockModels, AECSBlocks.SIMPLE_PATTERN_PROVIDER_BLOCK);
-        genPatternProviderLike(blockModels, AECSBlocks.RESONATING_PATTERN_PROVIDER_BLOCK);
-        genPatternProviderLike(blockModels, AECSBlocks.EX_RESONATING_PATTERN_PROVIDER_BLOCK);
+        genPatternProviderLike(blockModels, AECSBlocks.INTEGRATED_INTERFACE_BLOCK, "block/integrated_interface");
+        genPatternProviderLike(blockModels, AECSBlocks.EX_INTEGRATED_INTERFACE_BLOCK, "block/extended_integrated_interface");
+        genPatternProviderLike(blockModels, AECSBlocks.METEORITE_PATTERN_PROVIDER_BLOCK, "block/meteorite_pattern_provider");
+        genPatternProviderLike(blockModels, AECSBlocks.SIMPLE_PATTERN_PROVIDER_BLOCK, "block/simple_pattern_provider");
+        genPatternProviderLike(blockModels, AECSBlocks.RESONATING_PATTERN_PROVIDER_BLOCK, "block/resonating_pattern_provider");
+        genPatternProviderLike(blockModels, AECSBlocks.EX_RESONATING_PATTERN_PROVIDER_BLOCK, "block/extended_resonating_pattern_provider");
         genSixFaceLike(blockModels, AECSBlocks.CRYSTAL_GROWTH_CHAMBER_BLOCK.get());
         genSixFaceLike(blockModels, AECSBlocks.CRYSTAL_VIBRATION_CHAMBER_BLOCK.get());
         genSixFaceLike(blockModels, AECSBlocks.QUARTZ_GRINDSTONE_BLOCK.get());
@@ -69,11 +67,11 @@ public class AECSModelProvider extends ModelProvider {
         genSixFaceLike(blockModels, AECSBlocks.CRYSTAL_AGGREGATOR_BLOCK.get());
         genSixFaceLike(blockModels, AECSBlocks.ENTROPY_VARIATION_REACTION_CHAMBER_BLOCK.get());
         genSixFaceLike(blockModels, AECSBlocks.QUARTZ_OSCILLATOR_CLOCK_BLOCK.get());
-//        genEnderBroadcaster();
-//        genEnderEmitter();
+        genEnderBroadcaster(blockModels);
+        genEnderEmitter(blockModels);
 
-        blockWithItem(blockModels, AECSBlocks.ENDER_INTERFACE_BLOCK, ModelLocationUtils.getModelLocation(AECSBlocks.ENDER_INTERFACE_BLOCK.get(), "/base"));
-        blockWithItem(blockModels, AECSBlocks.EX_ENDER_INTERFACE_BLOCK, ModelLocationUtils.getModelLocation(AECSBlocks.ENDER_INTERFACE_BLOCK.get(), "/extended"));
+        blockWithItem(blockModels, AECSBlocks.ENDER_INTERFACE_BLOCK, ModelTemplates.CUBE_ALL.create(AECSBlocks.ENDER_INTERFACE_BLOCK.get(), TextureMapping.cube(new Material(AE2CrystalScience.makeId("block/ender_interface/base"))), blockModels.modelOutput));
+        blockWithItem(blockModels, AECSBlocks.EX_ENDER_INTERFACE_BLOCK, ModelTemplates.CUBE_ALL.create(AECSBlocks.EX_ENDER_INTERFACE_BLOCK.get(), TextureMapping.cube(new Material(AE2CrystalScience.makeId("block/ender_interface/extended"))), blockModels.modelOutput));
 
         //item
         for (DeferredItem<CrystalSeedItem> item : AECSItems.getCrystalSeeds()) {
@@ -95,17 +93,6 @@ public class AECSModelProvider extends ModelProvider {
         return BuiltInRegistries.ITEM.getKey(item.asItem()).getPath();
     }
 
-    /**
-     * 把非本模组命名空间的纹理标记为“已生成”，从而绕过存在性校验
-     */
-//    private void allowExternalTexture(String path)
-//    {
-//        Identifier rl = Identifier.parse(path);
-//        if (!rl.getNamespace().equals(AECSConstants.MODID))
-//        {
-//            this.existingFileHelper.trackGenerated(rl, ModelProvider.TEXTURE);
-//        }
-//    }
     public void crystalSeedItem(ItemModelGenerators itemModels, Item item) {
         var model0 = ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(item, "_0"));
         var model1 = ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(item, "_1"));
@@ -130,21 +117,24 @@ public class AECSModelProvider extends ModelProvider {
 
     protected void blockWithItem(BlockModelGenerators blockModels, DeferredBlock<?> deferredBlock, Identifier model) {
         var block = deferredBlock.get();
-        blockModels.createTrivialCube(block);
+        blockModels.blockStateOutput.accept(
+                MultiVariantGenerator.dispatch(block, BlockModelGenerators.plainVariant(model))
+        );
         blockModels.registerSimpleItemModel(block, model);
     }
 
     /**
-     * @param block    类似样板供应器的方块
-     * @param basePath 如：block/integrated_interface，随后你需要保证 block/integrated_interface/ 中包含其贴图
-     *                 （base、alternate、alternate_front、alternate_arrow）
+     * @param deferredBlock 类似样板供应器的方块
+     * @param basePath      如：block/integrated_interface，随后你需要保证 block/integrated_interface/ 中包含其贴图
+     *                      （base、alternate、alternate_front、alternate_arrow）
      */
-    private void genPatternProviderLike(BlockModelGenerators blockModels, DeferredBlock<?> deferredBlock) {
-        // 普通 cubeAll 模型（所有面同贴图）
+    private void genPatternProviderLike(BlockModelGenerators blockModels, DeferredBlock<?> deferredBlock, String basePath) {
         var block = deferredBlock.get();
-        var normalModel = TexturedModel.CUBE.create(block, blockModels.modelOutput).withSuffix("/base");
-//        blockModels.createTrivialCube(block);
-//        blockModels.registerSimpleItemModel(block, ModelLocationUtils.getModelLocation(block, "/base"));
+
+        // 物品模型生成
+        Identifier textureBase = AE2CrystalScience.makeId(basePath + "/base");
+        var normalModel = ModelTemplates.CUBE_ALL.create(block, TextureMapping.cube(new Material(textureBase)), blockModels.modelOutput);
+        blockModels.itemModelOutput.accept(block.asItem(), ItemModelUtils.plainModel(normalModel));
 
         // 自动生成 oriented 模型
         var textures = TextureMapping.particle(new Material(ModelLocationUtils.getModelLocation(block, "/base")))
@@ -214,7 +204,7 @@ public class AECSModelProvider extends ModelProvider {
     /**
      * 生成六个面各有不同贴图的机器，包括其模型、方块状态、方块物品模型
      * <p>
-     * 你需要保证“path/方块路径”下包含{@link #genSixFaceModel(Block, String, String, String)}中指定的6个纹理图片(bottom,top,front,back,left,right)；
+     * 你需要保证“path/方块路径”下包含{@link #genSixFaceModel(BlockModelGenerators, Block, String, String, String)}中指定的6个纹理图片(bottom,top,front,back,left,right)；
      * 此外，如果switchable为true，你应该在“path/方块路径/on”与“path/方块路径/off”分别提供对应的两套纹理，以对应机器打开与关闭状态下的模型纹理
      *
      * @param block      方块
@@ -222,6 +212,8 @@ public class AECSModelProvider extends ModelProvider {
      * @param switchable 该方块是否可开关，如果是，你应该提供on和off两套纹理
      */
     public void genSixFaceLike(BlockModelGenerators blockModels, Block block, @Nullable IOrientationStrategy strategy, boolean switchable) {
+        String blockPath = BuiltInRegistries.BLOCK.getKey(block).getPath();
+
         PropertyDispatch<VariantMutator> directionDispatch = null;
         if (strategy == OrientationStrategies.full()) {
             directionDispatch = createFacingSpinDispatch(0, 0);
@@ -229,25 +221,28 @@ public class AECSModelProvider extends ModelProvider {
             directionDispatch = createHorizontalFacingDispatch(0);
         }
 
-        MultiVariantGenerator gen;
+        Identifier offOrBaseModel = switchable
+                ? genSixFaceModel(blockModels, block, "/off", "block/" + blockPath + "/off")
+                : genSixFaceModel(blockModels, block, "", "block/" + blockPath);
 
-        if (switchable) {
-            var onModel = genSixFaceModel(blockModels, block, "/on");
-            var offModel = genSixFaceModel(blockModels, block, "/off");
-            gen = MultiVariantGenerator.dispatch(block)
-                    .with(PropertyDispatch.initial(AECSBlockProperties.ACTIVE)
-                            .select(false, BlockModelGenerators.plainVariant(offModel))
-                            .select(true, BlockModelGenerators.plainVariant(onModel)));
-        } else {
-            var model = genSixFaceModel(blockModels, block, "");
-            gen = MultiVariantGenerator.dispatch(block, BlockModelGenerators.plainVariant(model));
-        }
+        Identifier onModel = switchable
+                ? genSixFaceModel(blockModels, block, "/on", "block/" + blockPath + "/on")
+                : null;
+
+        var gen = MultiVariantGenerator.dispatch(block, BlockModelGenerators.plainVariant(offOrBaseModel));
 
         if (directionDispatch != null) {
             gen = gen.with(directionDispatch);
         }
 
+        if (switchable) {
+            gen = gen.with(PropertyDispatch.modify(AECSBlockProperties.ACTIVE)
+                    .select(false, BlockModelGenerators.NOP)
+                    .select(true, VariantMutator.MODEL.withValue(onModel)));
+        }
+
         blockModels.blockStateOutput.accept(gen);
+        blockModels.registerSimpleItemModel(block, offOrBaseModel);
     }
 
     /**
@@ -280,18 +275,19 @@ public class AECSModelProvider extends ModelProvider {
      * - 最终会产出一个模型路径为 方块注册路径 + modelPathSuffix 的模型
      * - 六个面的纹理都需要在texturePath中，例如block/crystal_vibration_chamber/(bottom,top,front,back,left,right)
      */
-    public Identifier genSixFaceModel(BlockModelGenerators blockModels, Block block, String modelPathSuffix) {
+    public Identifier genSixFaceModel(BlockModelGenerators blockModels, Block block, String modNameSpace, String modelPathSuffix, String texturePath) {
         var model = ModelLocationUtils.getModelLocation(block, modelPathSuffix);
-        var bottom = model.withSuffix("/bottom");
-        var top = model.withSuffix("/top");
-        var front = model.withSuffix("/front");
-        var back = model.withSuffix("/back");
-        var left = model.withSuffix("/left");
-        var right = model.withSuffix("/right");
+        var bottom = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/bottom");
+        var top = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/top");
+        var front = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/front");
+        var back = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/back");
+        var left = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/left");
+        var right = Identifier.fromNamespaceAndPath(modNameSpace, texturePath + "/right");
 
         return ModelTemplates.CUBE.create(
-                block,
+                model,
                 new TextureMapping()
+                        .put(TextureSlot.PARTICLE, new Material(front))
                         .put(TextureSlot.BOTTOM, new Material(bottom))
                         .put(TextureSlot.TOP, new Material(top))
                         .put(TextureSlot.NORTH, new Material(front))
@@ -301,59 +297,57 @@ public class AECSModelProvider extends ModelProvider {
                 blockModels.modelOutput);
     }
 
+    /**
+     * 按照一定规则生成六面模型（不包含方块状态）
+     * - 最终会产出一个模型路径为 方块注册路径 + _modelPathSuffix 的模型
+     * - 六个面的纹理都需要在texturePath中，例如block/crystal_vibration_chamber/(bottom,top,front,back,left,right,particle)
+     */
+    public Identifier genSixFaceModel(BlockModelGenerators blockModels, Block block, String modelPathSuffix, String texturePath) {
+        return genSixFaceModel(blockModels, block, AECSConstants.MODID, modelPathSuffix, texturePath);
+    }
+
     // 以下是特殊方块模型生成------------------------------------------------------------------------------------------------
 
     /**
      * 生成末影广播装置
      */
-//    private void genEnderBroadcaster() {
-//        Block block = AECSBlocks.ENDER_BROADCASTER_BLOCK.get();
-//        // 核心由BER渲染
-//        var formModel = models().getExistingFile(modLoc("block/me_ender_broadcaster/form"));
-//
-//        getVariantBuilder(block).forAllStates(state -> {
-//            return ConfiguredModel.builder()
-//                    .modelFile(formModel)
-//                    .build();
-//        });
-//
-//        simpleBlockItem(block, formModel);
-//    }
+    private void genEnderBroadcaster(BlockModelGenerators blockModels) {
+        Block block = AECSBlocks.ENDER_BROADCASTER_BLOCK.get();
+        // 核心由BER渲染
+        Identifier formModel = AE2CrystalScience.makeId("block/me_ender_broadcaster/form");
+
+        blockModels.blockStateOutput.accept(
+                MultiVariantGenerator.dispatch(block, BlockModelGenerators.plainVariant(formModel))
+        );
+
+        blockModels.registerSimpleItemModel(block, formModel);
+    }
 
     /**
      * 生成末影发信器
      */
-//    private void genEnderEmitter()
-//    {
-//        Block block = AECSBlocks.ENDER_EMITTER_BLOCK.get();
-//        var offModel = models().getExistingFile(modLoc("block/me_ender_emitter/off_form"));
-//        var onModel = models().getExistingFile(modLoc("block/me_ender_emitter/on_form"));
-//        var itemModel = models().getExistingFile(modLoc("block/me_ender_emitter/item"));
-//        var upperModel = models().getExistingFile(modLoc("block/me_ender_emitter/upper"));
-//
-//        getVariantBuilder(block).forAllStates(state -> {
-//            boolean active = state.getValue(AECSBlockProperties.ACTIVE);
-//            boolean upper = state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.UPPER;
-//
-//            ModelFile model;
-//            if (!active)
-//            {
-//                model = offModel;
-//            }
-//            else
-//            {
-//                model = onModel;
-//            }
-//
-//            if (upper)
-//                model = upperModel;
-//
-//
-//            return ConfiguredModel.builder()
-//                    .modelFile(model)
-//                    .build();
-//        });
-//
-//        simpleBlockItem(block, itemModel);
-//    }
+    private void genEnderEmitter(BlockModelGenerators blockModels) {
+        Block block = AECSBlocks.ENDER_EMITTER_BLOCK.get();
+        Identifier offModel = AE2CrystalScience.makeId("block/me_ender_emitter/off_form");
+        Identifier onModel = AE2CrystalScience.makeId("block/me_ender_emitter/on_form");
+        Identifier itemModel = AE2CrystalScience.makeId("block/me_ender_emitter/item");
+        Identifier upperModel = AE2CrystalScience.makeId("block/me_ender_emitter/upper");
+
+        blockModels.blockStateOutput.accept(
+                MultiVariantGenerator.dispatch(block)
+                        .with(PropertyDispatch.initial(
+                                AECSBlockProperties.ACTIVE,
+                                BlockStateProperties.HORIZONTAL_FACING,
+                                BlockStateProperties.DOUBLE_BLOCK_HALF
+                        ).generate((active, facing, half) -> {
+                            if (half == DoubleBlockHalf.UPPER) {
+                                return BlockModelGenerators.plainVariant(upperModel);
+                            }
+
+                            return BlockModelGenerators.plainVariant(active ? onModel : offModel);
+                        }))
+        );
+
+        blockModels.registerSimpleItemModel(block, itemModel);
+    }
 }
