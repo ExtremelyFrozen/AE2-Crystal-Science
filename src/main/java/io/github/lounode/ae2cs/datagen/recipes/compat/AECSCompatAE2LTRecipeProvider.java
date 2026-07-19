0fd637030eef88f947169e056fb4a07e346eb2c4
QiuYe <qiuye2024github@163.com>
2026-07-19 12:32:46 +0800

    修正AE2LT水晶配方

--- src/main/java/io/github/lounode/ae2cs/datagen/recipes/compat/AECSCompatAE2LTRecipeProvider.java
+++ src/main/java/io/github/lounode/ae2cs/datagen/recipes/compat/AECSCompatAE2LTRecipeProvider.java
@@ -1,6 +1,5 @@
 package io.github.lounode.ae2cs.datagen.recipes.compat;
 
-import io.github.lounode.ae2cs.AE2CrystalScience;
 import io.github.lounode.ae2cs.api.ids.AECSConstants;
 import io.github.lounode.ae2cs.common.init.AECSBlocks;
 import io.github.lounode.ae2cs.datagen.AECSRecipeProvider;
@@ -11,6 +10,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -32,14 +32,11 @@ public class AECSCompatAE2LTRecipeProvider extends AECSRecipeProvider {
         var compatOut = originalOut.withConditions(modLoaded(AECSConstants.AE2LT_ID));
         super.buildRecipes(compatOut, registries);
 
-        var overloadCrystalId = ResourceLocation.fromNamespaceAndPath(AECSConstants.AE2LT_ID, "charged_overload_crystal");
-        var overloadCrystal = BuiltInRegistries.ITEM.getOptional(overloadCrystalId);
-        if (overloadCrystal.isEmpty()) {
-            AE2CrystalScience.LOGGER.warn("Skipping AE2 Lightning Tech compatibility recipes because required item {} is not registered", overloadCrystalId);
-            return;
-        }
-
         packAndUnpack2x2(compatOut, RecipeCategory.MISC, RecipeCategory.MISC,
-                overloadCrystal.get(), AECSBlocks.CHARGED_OVERLOAD_CRYSTAL_BLOCK);
+                externalItem(AECSConstants.AE2LT_ID, "overload_crystal"), AECSBlocks.CHARGED_OVERLOAD_CRYSTAL_BLOCK);
+    }
+
+    private static Item externalItem(String namespace, String path) {
+        return BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath(namespace, path));
     }
 }
